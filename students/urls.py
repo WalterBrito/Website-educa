eb6644fab0c6c6a889ff88cfacadaaa4efc9ff96
WalterBrito <waltter.britto@gmail.com>
2016-08-03 16:40:17 -0300

    Website e-learning plataform

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,7 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^register/$',
+        views.StudentRegistrationView.as_view(),
         name='student_registration'),
     url(r'^enroll-course/$',
         views.StudentEnrollCourseView.as_view(),
